19543be3a03833d3ae9c6c30545172dbad96a8be
Pconti10 <pconti10@gmail.com>
2022-07-16 17:20:58 -0300

    terminando index

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ new TypeIt("#simpleUsage", {
     speed: 70,
     waitUntilVisible: true,
   }).go();
+
+
+  var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 20000,
+  wrap: false
+})
